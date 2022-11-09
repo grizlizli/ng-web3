85f9cc1ff3960f34757fd2cb89cce30c20172dba
Miloš Krstić <mrkrsticmilos@gmail.com>
2022-11-09 12:57:13 +0100

    inital ng app with web3 packages

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng-web3-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ng-web3';
+}
